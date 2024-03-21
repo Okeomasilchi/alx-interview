1734ccf782b2eb7fb5b6c6aeec5e6984496db626
okeomasilchi <okeomasilachi@gmail.com>
2024-03-22 00:20:02 +0100

    Refactor minOperations function to improve readability and efficiency

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,29 +7,25 @@ Copy All and Paste.
 """
 
 
+#!/usr/bin/python3
+"""
+Defines a method that calculates the fewest number of operations needed
+to result in exactly n H characters in the file, given two operations: Copy All
+and Paste.
+"""
+
+
 def minOperations(n):
-    """
-    Calculates the fewest number of operations
-    needed to result in exactly n H character
-    in the file.
-
-    Args:
-      n (int): The target number of H characters.
-
-    Returns:
-      int: The minimum number of operations.
-    """
+    """Calculates the fewest number of operations
+    needed to reach n H characters."""
     if n <= 1:
         return 0
 
     operations = 0
     divisor = 2
-
     while n > 1:
-        if n % divisor == 0:
+        while n % divisor == 0:
             operations += divisor
             n //= divisor
-        else:
-            divisor += 1
-
+        divisor += 1
     return operations
