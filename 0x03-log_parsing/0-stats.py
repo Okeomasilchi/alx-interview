b24c28dbbaa375fa8212007220107a7b055e3a88
okeomasilchi <okeomasilachi@gmail.com>
2024-03-29 01:12:58 +0100

    Update status_code_count initialization

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,7 +14,7 @@ pattern = re.compile(
 
 # Initialize variables
 total_file_size = 0
-status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}  # noqa: E501
 line_count = 0
 
 try:
