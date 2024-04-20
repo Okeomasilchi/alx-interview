ca38721fec302dbdf37fb6acf52862e0a4f8e327
okeomasilchi <okeomasilachi@gmail.com>
2024-04-20 16:48:06 +0100

    Refactor Star Wars API code to use async/await for character requests

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,42 @@
 #!/usr/bin/node
+// Import the necessary modules
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const { characters } = JSON.parse(body);
-    for (let index = 0; index < characters.length; index++) {
-      request(characters[index], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
+
+function requestSync (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ response, body });
+      }
+    });
+  });
+}
+
+async function fetchMovieData (movieId) {
+  try {
+    const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+    const { response, body } = await requestSync(movieUrl);
+
+    if (response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      const characters = movieData.characters;
+
+      for (const url of characters) {
+        const { response: charResponse, body: charBody } = await requestSync(url);
+        if (charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
         }
-      });
+      }
     }
+  } catch (error) {
+    console.error('Error:', error.message);
   }
-});
+}
+
+const movieId = process.argv[2];
+
+fetchMovieData(movieId);
