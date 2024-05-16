c21e49661fdb7015a8a2a77c1ef98e6149d48c1d
okeomasilachi onyedibia <110942635+okeomasilachi@users.noreply.github.com>
2024-05-16 21:39:02 +0000

    Refactor isWinner function for improved readability and maintainability

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,80 @@
 #!/usr/bin/python3
 """
-Prime Game for Alx Intervieew
+Prime Game for Alx Interview
+
+This module contains functions related to a prime game. The
+game is played between two players, Maria and Ben. The game
+takes a list of numbers as input and determines the winner based
+on a specific set of rules.
 """
 
-from typing import List, Optional
 
-
-def isWinner(x: int, nums: List[int]) -> Optional[str]:
+def is_prime(n):
     """
-    Determines the winner of a game played over multiple
-    rounds.
+    Check if a number is prime.
 
-    Parameters:
-    - x (int): The number of rounds played.
-    - nums (List[int]): A list containing the maximum
-        integer for each round.
+    Args:
+        n (int): The number to check.
 
     Returns:
-    - Optional[str]: The name of the player who won the most
-        rounds ('Maria' or 'Ben'). Returns 'None' if there is
-        a tie.
+        bool: True if the number is prime, False otherwise.
+    """
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-    The function implements the Sieve of Eratosthenes algorithm
-    for each round and decides the winner based on the parity of
-    the count of remaining primes.
+
+def isWinner(x, nums):
+    """
+    Determines the winner of a prime game.
+
+    Args:
+        x (int): The number of rounds to be played.
+        nums (List[int]): A list of integers representing
+            the number of elements in each round.
+
+    Returns:
+        str or None: The name of the winner (either "Maria"
+        or "Ben") or None if there is no winner.
     """
     maria_wins = 0
     ben_wins = 0
-
     for n in nums:
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-
-        remaining_primes = [i for i in range(2, n + 1) if primes[i]]
-
-        if len(remaining_primes) % 2 == 0:
+        remaining = list(range(1, n + 1))
+        is_prime_list = [False] * (n + 1)
+        for i in range(2, n + 1):
+            if is_prime(i):
+                is_prime_list[i] = True
+        maria_turn = True
+        while True:
+            found_prime = False
+            for num in remaining:
+                if is_prime_list[num]:
+                    prime = num
+                    found_prime = True
+                    break
+            if not found_prime:
+                break
+            for multiple in range(prime, n + 1, prime):
+                if multiple in remaining:
+                    remaining.remove(multiple)
+            if maria_turn:
+                maria_turn = False
+            else:
+                maria_turn = True
+        if maria_turn:
             ben_wins += 1
         else:
             maria_wins += 1
-
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
